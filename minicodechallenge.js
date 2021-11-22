937d61b884087ca8158236086ca21d1c41202538
narcis <narcissprecic@gmail.com>
2021-11-22 16:45:50 -0500

    Updated to now functioning, but only works in the positive quadrant

--- minicodechallenge.js
+++ minicodechallenge.js
@@ -1,10 +1,26 @@
-function logRectInfo(topLeft, bottomRight){  
-    a = topLeft[1] * bottomRight[0];
-    p = 2 * (topLeft[1] + bottomRight[0]);
+function logRectInfo(topLeft, bottomRight){ //This only works for positive cordinates
+    a = (topLeft[1] - bottomRight[1]) * (bottomRight[0] - topLeft[0]);
+    p = 2 * ((topLeft[1] - bottomRight[1]) + (bottomRight[0] - topLeft[0]));
+
+  
     console.log("Your area is: " + a + " Your parameter is: " + p);
   }
   
   logRectInfo([0,5], [3,2]);
-  logRectInfo([0,3], [4,1]);
-  logRectInfo([0,8], [3,2]);
-  logRectInfo([1,5], [3,1]);
+
+
+  
+  /* Me tring to figure it out for all quadrants
+  if(topLeft[0] < 0 || bottomRight[0] < 0){
+    a = (topLeft[1] - bottomRight[1]) * (Math.abs(Math.abs(bottomRight[0]) - Math.abs(topLeft[0])));
+    p = 2 * (topLeft[1] - bottomRight[1]) + ((Math.abs(bottomRight[0]) - Math.abs(topLeft[0]))); //()
+  } else{
+    a = (topLeft[1] - bottomRight[1]) * (bottomRight[0] - topLeft[0]);
+    p = 2 * (topLeft[1] - bottomRight[1]) + (bottomRight[0] - topLeft[0]);
+
+  }
+  
+    console.log("Your area is: " + a + " Your parameter is: " + p);
+  }
+*/ 
+ 
